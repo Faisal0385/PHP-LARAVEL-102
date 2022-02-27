79912fd7249dd23572322673b88bcfc2bb2bfa56
Faisal A. Salam <faisaltez@gmail.com>
2022-02-27 15:50:24 +0600

    Class 7

---
+++ Class 6 - form data collections 2/about.php
@@ -0,0 +1,18 @@
+<?php
+include './components/header/header.php';
+?>
+
+<div class="container pt-3">
+    <div class="row">
+        <div class="col-lg-6">
+            <img src="./assets/images/5.png" alt="">
+        </div>
+        <div class="col-lg-6">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, aliquam placeat aspernatur soluta assumenda in nobis odio numquam quia, eligendi nesciunt consectetur modi commodi? Dolorum ipsum reiciendis magni quo dignissimos.F
+        </div>
+    </div>
+</div>
+
+<?php
+include './components/footer/footer.php';
+?>
